873517e439941bd373f0f1c503ddea5946fc3d8e
Naveenprakash <naveenprakashjk24@gmail.com>
2021-11-29 08:39:33 +0530

    message updates

--- baseApp/views.py
+++ baseApp/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse 
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.db.models import Q
 from django.contrib import messages
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from django.contrib.auth.models import User
 from .forms import RoomForm
 
@@ -23,7 +24,7 @@ def loginPage(request):
     if request.user.is_authenticated:
         return redirect('home')
     if request.method=='POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
         try:
             user = User.objects.get(username=username)
@@ -44,9 +45,19 @@ def logoutUser(request):
     return redirect('home')
 
 def registerPage(request):
-    page= 'register'
-    context={'page':page}
-    return render(request, 'baseApp/login_register.html',context)
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occurred during registertion')
+    return render(request, 'baseApp/login_register.html',{'form':form})
 
 
 def home(request):
@@ -59,7 +70,16 @@ def home(request):
 
 def room(request,pk):
     room=Room.objects.get(id=pk)
-    context = {'room':room}
+    room_messages = room.message_set.all().order_by('-cretaed')
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body')
+        )
+        return redirect('room', pk=room.id)
+
+    context = {'room':room,'room_messages':room_messages}
     return render(request, 'baseApp/room.html', context)
 
 
